7c56f4a4a8a5374c816eeece37284b96f6782117
marcinwasowicz <marcinkwasowicz@gmail.com>
2022-11-20 09:44:03 +0100

    Implement sensitivity analysis

--- handy/handy_sa_analysis.py
+++ handy/handy_sa_analysis.py
@@ -2,7 +2,11 @@ import numpy as np
 from SALib.sample import saltelli
 from SALib.analyze import sobol
 
-from handy.handy_prototype import HANDY_PARAMETER_SYMBOLS, simulate_handy
+from handy.handy_prototype import (
+    HANDY_PARAMETER_SYMBOLS,
+    HANDY_VARIABLES_SYMBOLS,
+    simulate_handy,
+)
 
 
 def sensitivity_analysis(
@@ -10,12 +14,37 @@ def sensitivity_analysis(
     initial_value,
     differential_t,
     simulation_steps,
-    parameters_sample_count,
 ):
     problem = {
         "num_vars": len(HANDY_PARAMETER_SYMBOLS),
         "names": HANDY_PARAMETER_SYMBOLS,
         "bounds": parameters_sa_bounds,
     }
-    # TODO: Implement sobol SA analysis
-    pass
+    sample_parameters = saltelli.sample(problem, 256, calc_second_order=False)
+    simulations = []
+    for parameters in sample_parameters:
+        try:
+            simulations.append(
+                simulate_handy(
+                    initial_value,
+                    parameters,
+                    differential_t,
+                    simulation_steps,
+                    ignore_errors=False,
+                ).T
+            )
+        except:
+            simulations.append(simulations[-1])
+    simulations = np.array(simulations)
+
+    return {
+        symbol: np.array(
+            [
+                sobol.analyze(
+                    problem, simulations[:, symbol_idx, t], calc_second_order=False
+                )["S1"]
+                for t in range(simulations.shape[-1])
+            ]
+        )
+        for symbol_idx, symbol in enumerate(HANDY_VARIABLES_SYMBOLS)
+    }
